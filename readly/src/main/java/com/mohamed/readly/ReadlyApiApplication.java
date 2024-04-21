a091894ebe9210c6a5577999927447fa1f425cfc
MohamedBoukthir <medboukthir7@gmail.com>
2024-04-21 18:29:15 +0100

    Initial commit

---
+++ readly/src/main/java/com/mohamed/readly/ReadlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.readly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadlyApiApplication.class, args);
+	}
+
+}
---
+++ readly/src/test/java/com/mohamed/readly/ReadlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.readly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
