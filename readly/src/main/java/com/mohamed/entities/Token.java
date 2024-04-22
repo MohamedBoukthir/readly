b50ad4ea3108f8a4ac6cb35b5ca050a54ae04658
MohamedBoukthir <medboukthir7@gmail.com>
2024-04-22 13:12:47 +0100

    add: token entity repository

---
+++ readly/src/main/java/com/mohamed/entities/Token.java
@@ -0,0 +1,30 @@
+package com.mohamed.entities;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
---
+++ readly/src/main/java/com/mohamed/repositories/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.mohamed.repositories;
+
+import com.mohamed.entities.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+}
