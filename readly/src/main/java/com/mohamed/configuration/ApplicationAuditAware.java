745266298e70648691b9f09596fbc95e8596b6bb
MohamedBoukthir <medboukthir7@gmail.com>
2024-04-28 11:50:16 +0100

    add: application auditor aware

---
+++ readly/src/main/java/com/mohamed/configuration/ApplicationAuditAware.java
@@ -0,0 +1,25 @@
+package com.mohamed.configuration;
+
+import com.mohamed.entities.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (
+                        authentication == null
+                        || !authentication.isAuthenticated()
+                        || authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+        User user = (User) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
