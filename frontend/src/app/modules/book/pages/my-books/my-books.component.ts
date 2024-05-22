26ef7028fe418cafe860ec9f773cc46f995507e3
MohamedBoukthir <medboukthir7@gmail.com>
2024-05-22 23:16:17 +0100

    implement: archive book functionality

--- frontend/src/app/modules/book/pages/my-books/my-books.component.ts
+++ frontend/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -67,7 +67,13 @@ export class MyBooksComponent implements OnInit {
   }
 
   archiveBook(book: BookResponse) {
-
+    this.bookService.updateArchivedStatus({
+      'book-id': book.id as number,
+    }).subscribe({
+      next: () => {
+        book.archived = !book.archived;
+      }
+    });
   }
 
   shareBook(book: BookResponse) {
