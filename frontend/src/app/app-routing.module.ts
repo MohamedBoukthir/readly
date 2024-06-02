491520d263ef660a4cc804f783de4fec2c350475
MohamedBoukthir <medboukthir7@gmail.com>
2024-06-02 01:43:45 +0100

    fix: update dockerfile to correctly copy angular build output

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
-import {HomeComponent} from "./pages/home/home.component";
 import {authGuard} from "./services/guard/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'activate-account', component: ActivateAccountComponent},
